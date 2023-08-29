f3ea15dc4ab9191f03b61e6f10c1774c1f1088c8
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2023-08-29 14:26:52 -0600

    efecto scroll en jquery

--- js/main.js
+++ js/main.js
@@ -1,125 +1,13 @@
-
-<body>
-<div class="container">
-    <!-- info -->
-    <section class="us">
-        <div class="aboutUs">
-            <!-- img -->
-            <div class="aboutUs-img">
-                <img src="./img/Solary-color.png" alt="">
-            </div>
-            <!-- descripcion -->
-            <article>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus culpa eos tempore, labore
-                    veritatis ad quaerat perferendis ut dignissimos veniam molestias sequi nam deserunt est dolores
-                    ipsum nesciunt laudantium illo?
-                </p>
-            </article>
-        </div>
-    </section>
-    <!--celebracion-->
-    <section class="celebration">
-        <div class="celebration-desc">
-            <h3>CELEBRA CON NOSOTROS</h3>
-            <p>En Salón de Fiestas SOLARY te ofrecemos las instalaciones para los siguientes tipos de eventos </p>
-        </div>
-        <!-- cards -->
-        <div class="container-cards">
-            <div class="container-card_p">
-                <!-- cumpleaños -->
-                <div class="cards">
-                    <!-- image -->
-                    <div class="cards-img">
-                        <img src="./img/CUMPLEAÑOS.png" alt="">
-                    </div>
-                    <!-- descripcion -->
-                    <p>Cumpleaños</p>
-                </div>
-                <!-- xv -->
-                <div class="cards">
-                    <!-- image -->
-                    <div class="cards-img">
-                        <img src="./img/xv.png" alt="">
-                    </div>
-                    <!-- descr -->
-                    <p>XV Años</p>
-                </div>
-                <!--infantiles -->
-                <div class="cards">
-                    <!-- image -->
-                    <div class="cards-img">
-                        <img src="./img/infantiles.png" alt="">
-                    </div>
-                    <!-- descripcion -->
-                    <p>Fiestas Infantiles</p>
-                </div>
-            </div>
-
-            <div class="container-card_p">
-                <!--baby-->
-                <div class="cards">
-                    <div class="cards-content">
-                        <!-- image -->
-                        <div class="cards-content-img">
-                            <img src="./img/baby.png" alt="">
-                        </div>
-                        <!-- descripcion -->
-                        <h4>Baby-Shower</h4>
-                    </div>
-                </div>
-                <!--bautizo-->
-                <div class="cards">
-                    <!-- image -->
-                    <div class="cards-img">
-                        <img src="./img/bautizos.png" alt="">
-                    </div>
-                    <!-- descripcion -->
-                    <p>Bautizos</p>
-                </div>
-                <!--confirmacion-->
-                <div class="cards">
-                    <!-- image -->
-                    <div class="cards-img">
-                        <img src="./img/confirmacion.png" alt="">
-                    </div>
-                    <!-- descripcion -->
-                    <p>Confirmaciones</p>
-                </div>
-            </div>
-        </div>
-        <!-- btn -->
-        <a href="">link</a>
-    </section>
-    <!--social media-->
-    <section class="media">
-        <!-- text -->
-        <div class="media-content">
-            <h3>SÍGUENOS EN IG <br>
-                Y HAZNOS PARTE DE LA FIESTA
-            </h3>
-            <!-- btn -->
-            <div class="media-content-btn">
-                <div class="btn-media">
-                    <a href="">
-                        <img src="" alt="">
-                    </a>
-                </div>
-            </div>
-        </div>
-        <!-- content-img -->
-        <div class="media-content-images">
-
-        </div>
-    </section>
-</div>
-</body>
-
-<footer>
-<div class="container-ft">
-    <div class="ft-imagen">
-        <img src="./img/Solary-logo-blanco.png" alt="SolaryGarden-logo">
-    </div>
-    <p class="ft-text">© 2023 SOLARY GARDEN. Todos los derechos reservados.</p>
-</div>
-</footer>
+$(document).ready(function () {
+    //efcto scroll menu 
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        //condition for window 
+        if (scroll > 250) {
+            $(".navbar").css("background", "#5645");
+            // $('body').css("background","")
+        } else {
+            $(".navbar").css("background", "transparent");
+        }
+    });
+})
